ffadbc108c4e6240122b2a4434431388152e19fa
hdewan63 <65296546+hdewan63@users.noreply.github.com>
2025-10-14 14:57:11 +0530

    s

---
+++ calculator.py
@@ -0,0 +1,18 @@
+num1=10
+num2=5
+
+def addition():
+    add= num1+num2
+    print("The addition of two numbers is:",add)
+
+def subtraction():
+    sub= num1-num2 
+    print("The subtraction of two numbers is:",sub)
+
+def multiply():
+    mul= num1*num2
+    print("The multiplication of two numbers is:",mul)
+
+addition()
+subtraction()
+multiply()
